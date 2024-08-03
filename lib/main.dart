0a72a9887e7757cc54a4a9aeb15fa6b81f4908ce
sayed younis <elsaydseif17@gmail.com>
2024-08-03 17:42:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShopApp());
+}
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
